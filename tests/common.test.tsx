63ee53842598ac9b795432a82e4eee4e5eb5ce13
MingXue <ming@twotone.co>
2023-01-20 08:44:53 +0800

    update test tsx

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -3,10 +3,10 @@ import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
 
-import { SF_Form } from '../src'
+import { SignUpForm } from '../src'
 
 describe('Common render', () => {
   it('renders without crashing', () => {
-    render(<SF_Form />)
+    render(<SignUpForm />)
   })
 })
