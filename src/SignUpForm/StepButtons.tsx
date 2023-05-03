8f2dee697ac8f44e2ba71232b6f504f0461d2abb
ming <ming@twotone.co>
2023-05-03 09:59:28 +0800

    refactor for step submitting

--- src/SignUpForm/StepButtons.tsx
+++ src/SignUpForm/StepButtons.tsx
@@ -4,26 +4,22 @@ import { IStepButtons } from './types'
 import { BackButton, Badge, ButtonGroup, ButtonIconWrapper, Loader, StepButton, Text } from './styles'
 import { BadgeIcon, LockIcon, SubmitLoading } from './Icons'
 
-export const StepButtons = ({ setCurrentStep, currentStep, steps, isSubmiting }: IStepButtons) => {
-  const handdleBack = () => {
-    if (setCurrentStep) setCurrentStep(currentStep - 1)
-  }
-
+export const StepButtons = ({ currentStep, loading, back }: IStepButtons) => {
   return (
     <ButtonGroup marginTop={currentStep === 2} className='sf-sign-up-form-buttons'>
-      {currentStep === 2 && <BackButton onClick={handdleBack}>Back</BackButton>}
+      {currentStep === 2 && <BackButton onClick={back}>Back</BackButton>}
       <StepButton
         type='submit'
-        disabled={isSubmiting}
-        isSubmiting={isSubmiting}
+        disabled={loading}
+        isSubmitting={loading}
         form={currentStep === 1 ? 'stepOneForm' : 'stepTwoForm'}
       >
         <Badge>
           <BadgeIcon />
         </Badge>
-        <Text>{currentStep === 1 ? (steps === 1 ? 'Register' : 'Continue') : 'Make a payment'}</Text>
+        <Text>{currentStep === 1 ? 'Continue' : 'Make a payment'}</Text>
         <ButtonIconWrapper>{currentStep === 1 ? <ForwardIcon /> : <LockIcon />}</ButtonIconWrapper>
-        {isSubmiting && (
+        {loading && (
           <Loader>
             <SubmitLoading />
           </Loader>
--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as React from 'react'
 import { render } from '@testing-library/react'
 
@@ -9,11 +10,14 @@ describe('Common render', () => {
   it('renders without crashing', () => {
     render(
       <SignUpForm
-        onSubmit={function (): void {
-          throw new Error('Function not implemented.')
-        }}
         headingTitle={[]}
         subCaption={[]}
+        steps={1}
+        loading={false}
+        currentStep={0}
+        onSubmit={function (): Promise<void> {
+          throw new Error('Function not implemented.')
+        }}
       />,
     )
   })
